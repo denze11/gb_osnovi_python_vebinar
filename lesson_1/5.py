221bf17b189ae9fdd734691272b719657f25d708
Denze1 <raven1287@gmail.com>
2021-03-31 23:32:37 +0300

    Исправил задачу №5

--- lesson_1/5.py
+++ lesson_1/5.py
@@ -2,8 +2,14 @@ proceeds = int(input('Введите выручку: '))
 costs = int(input('Введите издержки: '))
 
 if proceeds - costs > 0:
-    print(f'У Вашей фирмы прибыль {proceeds - costs}')
+    profit = 100 * ( proceeds - costs) // proceeds
+    print(f'У вашей фирмы прибыль {profit}%')
+    worker = int(input('Введите количество работников: '))
+    w_profit = proceeds // worker
+    print(f'Прибыль на одного работника составила {w_profit} тысяч')
+
 elif proceeds - costs < 0:
-    print(f'У Вашей фирмы убыток "{proceeds - costs}"')
+    loss = 100 * (proceeds - costs) // costs
+    print(f'У вашей фирмы убыток "{loss}%"')
 else:
     print('Ваша фирма отработала в ноль')
